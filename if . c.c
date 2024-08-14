6e83c513a829317c1ae8d683d50b9efa5c52adbb
dnc12 <bhaleraodiksha30@gmail.com>
2024-08-14 11:11:19 +0530

    Add files via upload

---
+++ if . c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int a,b;
+clrscr();
+printf("\n Enter the two numbers");
+scanf("%d%d",&a&b);
+if (a>b)
+{
+printf("\n a is maximum");
+if (a>b)
+{
+printf("\n a is maximum");
+}
+printf("value of a is %d\n",a);
+getch();
+}
